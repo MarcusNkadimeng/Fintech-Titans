8c3ba7d5ea4399b9480cfad9fa66bf021a7ba6df
RockinRuby <rgroenewald@jhb.dvt.co.za>
2024-07-20 18:59:32 +0200

    little fix to sidebar

--- src/app/Sidebar.tsx
+++ src/app/Sidebar.tsx
@@ -7,14 +7,14 @@ const Sidebar = () => {
   const { isSidebarOpen, messages, setChatId, chatId, setIsSidebarOpen } = useHomeStore((state) => state);
 
   return isSidebarOpen ? (
-    <ul className="menu menu-md bg-slate-200 rounded-box w-96 absolute left-4 top-16 z-10 shadow-2xl">
-      {Object.entries(messages).map(((msg: any) => {
+    <ul className="menu menu-md bg-slate-200 rounded-box w-96 absolute left-2 top-16 z-10 shadow-2xl">
+      {Object.entries(messages).length > 0 ? Object.entries(messages).map(((msg: any) => {
         const msgChatId = msg[0];
         const msgs = messages[msg[0]];
         const lastMsg = msgs[msgs.length - 1];
         const isActive = chatId === msgChatId;
         return (<li><a onClick={() => { setChatId(msgChatId); setIsSidebarOpen(false); }} className={`${isActive ? "active" : ""}`}>{msgChatId} - {lastMsg.slice(0, 20)}</a></li>)
-      }))}
+      })) : <li className="disabled"><a>No chats found</a></li>}
     </ul>
   ) : null;
 };
